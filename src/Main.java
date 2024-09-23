86eec7a90e3d386159e609d95b9bfb48eb92d991
videogamelife8 <tyler.rivers2023@gmail.com>
2024-09-23 14:16:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.sql.SQLOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        int numToExamine = 56;
+
+
+        if (numToExamine % 2 == 0) {
+
+
+            System.out.println(numToExamine + " is even ");
+
+
+
+        }
+        else if (numToExamine % 2 == 1) {
+            System.out.println(numToExamine + " is odd ");
+
+        }
+        }
+    }
